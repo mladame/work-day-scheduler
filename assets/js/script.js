328f709e610f94f84e7a26df1bbc15c4f8be98cc
Martha A <mladame43@gmail.com>
2022-08-26 00:28:34 -0700

    code cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
 //     PSEUDOCODE - branch - <setup>
-//  upon open we are presented with:
-//      planner,  timeblocks.textarea for standard business hours - branch <feature/timeblocks>
-//      current date at top of calendar "Day, Month Date, Year" - branch <feature/time>
+
 //  timeblocks should be color coded for past, present, future - branch <feature/color-code>
 //  clicking on timeblock will prompt you to enter an event onto timeblock <feature/save-event>
 //      save button will save event to local storage
 //      events will persist upon refresh
-//  append timeblock + button to hour?
 
 //  Work Day Scheduler Code
 //  -------DEFINE ELEMENTS--------
 //      elements to be defined: 
-//x          "#currentDay" - container to render the date
 //          ".container" - will hold timeblocks
 //          ".time-block" - timeblocks in container under rows
 //          ".saveBtn" - in container under rows
@@ -28,18 +24,6 @@ $(document).ready(function(){
     var todaysDate = moment().format("dddd, MMMM Do YYYY");
     $('#currentDay').html(todaysDate);
 })
-
-//  function to call date upon open
-
-//  add moment.js format: moment().format("dddd, MMMM Do YYYY");
-//      UNIX Timestapm (seconds)    moment(1318874398806).unix(); // 1318874398
-//      moment(Number); var day = moment(1318781876406);    moment.unix(Number)
-//      moment().hour(Number);
-//      moment().hour(); // Number
-
-//      var d = new Date();
-
-//      var day = d.getDate();
 
 
 //  -------COLOR CODE--------
